760a7e540ca2b254ddec9d4bb6cd19c139c5c435
Oded Levi <avdadlevi@gmail.com>
2024-11-23 20:03:24 +0200

    Sanity

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { defineConfig, devices } = require('@playwright/test');
+import { defineConfig, devices } from '@playwright/test';
 
 /**
  * Read environment variables from file.
@@ -10,7 +10,7 @@ const { defineConfig, devices } = require('@playwright/test');
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
-module.exports = defineConfig({
+export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -19,16 +19,29 @@ module.exports = defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    [
+      'html',
+      {
+        open: 'never', outputFolder: 'playwright-report'
+      }, 
+    ],
+    ['list'],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
+    screenshot: {
+      mode: 'only-on-failure', 
+      fullPage: true,
+    },
+    video: 'retain-on-failure', 
   },
 
   /* Configure projects for major browsers */
@@ -43,10 +56,6 @@ module.exports = defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
 
     /* Test against mobile viewports. */
     // {
---
+++ tests/sanityTests/sanitytest.spec.js
@@ -0,0 +1,40 @@
+import { expect, test } from "@playwright/test";
+
+test.describe("sanity suite", () => {
+  test("sanity", async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/");
+    //Fill in
+    await page.locator('[data-test="username"]').fill("standard_user");
+    await page.locator('[data-test="password"]').fill("secret_sauce");
+    //click
+    await page.locator('[data-test="login-button"]').click();
+    //url validation
+    await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+    //adding to shopping cart
+    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+    await expect(page.locator('[data-test="shopping-cart-link"]')).toContainText('2');
+    await page.locator('[data-test="shopping-cart-link"]').click();
+    //validate and checkout
+    await expect(page).toHaveURL("https://www.saucedemo.com/cart.html");
+    await expect(page.locator('[data-test="secondary-header"]')).toContainText("Your Cart");
+    await expect(page.locator('[data-test="inventory-item"]')).toHaveCount(2);
+    await page.locator('[data-test="checkout"]').click();
+    //checkout page
+    await expect(page).toHaveURL("https://www.saucedemo.com/checkout-step-one.html");
+    await expect(page.locator('[data-test="secondary-header"]')).toContainText("Checkout: Your Information");
+    await page.locator('[data-test="firstName"]').fill('Oded');
+    await page.locator('[data-test="lastName"]').fill('Levi-Shafran');
+    await page.locator('[data-test="postalCode"]').fill('3086000');
+    await page.locator('[data-test="continue"]').click();
+    //checkout overview
+    await expect(page).toHaveURL("https://www.saucedemo.com/checkout-step-two.html");
+    await expect(page.locator('[data-test="title"]')).toContainText("Checkout: Overview");
+    await page.locator('[data-test="finish"]').click();
+    //checkout complete
+    await expect(page).toHaveURL("https://www.saucedemo.com/checkout-complete.html")
+    await expect(page.locator('[data-test="title"]')).toContainText("Checkout: Complete!");
+    await expect(page.locator('[data-test="complete-header"]')).toContainText("Thank you for your order!");
+    await expect(page.locator('[data-test="complete-text"]')).toContainText("Your order has been dispatched, and will arrive just as fast as the pony can get there!");
+  });
+});
